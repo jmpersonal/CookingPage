cb9743959986b582872bbe7e12e404a1e846db76
jmedina <devjmedina@gmail.com>
2021-01-06 16:27:30 -0600

    Validation

--- client/src/components/ViewRecipes/ViewEditRecipe.tsx
+++ client/src/components/ViewRecipes/ViewEditRecipe.tsx
@@ -17,7 +17,7 @@ interface Props {
 const EditRecipe: React.FC<Props> = ({ recipe }) => {
   const store = useSelector((state: {recipesStore: IStore})=> state.recipesStore);
   const dispatch = useDispatch();
-  const { register, handleSubmit, control } = useForm({defaultValues: recipe});
+  const { register, handleSubmit, control, errors } = useForm({defaultValues: recipe});
 
   const { fields: ingredientsField, append: ingredientsAppend, remove: removeIngredient, append: appendIngredient } = useFieldArray({control, name: "ingredients"})
   const { fields: directionsField, append: directionsAppend, remove: removeDirection, append: appendDirection } = useFieldArray({control, name: "directions"})
@@ -29,13 +29,17 @@ const EditRecipe: React.FC<Props> = ({ recipe }) => {
     }
   }, [])
 
+  useEffect(()=>{
+    console.log(errors.directions);
+  }, [errors])
+
   const onSubmit = (data: any) => {
-    console.log(data);
     
     let id = recipe.uuid
-    dispatch(initEditRecipe(id, data))
+    // dispatch(initEditRecipe(id, data))
   }
 
+  
   return (
     <>
     <RecipeItem recipe={recipe} fullview={true} />
@@ -52,7 +56,7 @@ const EditRecipe: React.FC<Props> = ({ recipe }) => {
           return (
             <div style={{width: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center'}} key={i.uuid}>
               <Input defaultValue={i.uuid} name={`ingredients[${idx}].uuid`} ref={register()} type="hidden"/>
-              <Input defaultValue={i.name} label={`Ingredient ${_idx}`} name={`ingredients[${idx}].name`} ref={register()}/>
+              <Input defaultValue={i.name} label={`Ingredient ${_idx}`} name={`ingredients[${idx}].name`} ref={register({required: true})}/>
               <Input defaultValue={i.measurement} label={`Measurement`} name={`ingredients[${idx}].measurement`} ref={register()} style={{width: '70px'}}/>
               <Input defaultValue={i.amount} label={`Amount`} name={`ingredients[${idx}].amount`} ref={register()} style={{width: '30px'}}/>
               <Button onClick={()=> removeIngredient(idx)} text="Remove">remove</Button>
@@ -65,7 +69,7 @@ const EditRecipe: React.FC<Props> = ({ recipe }) => {
           let _idx = idx + 1;
           return (
             <div style={{width: '100%', display: 'flex', flexDirection: 'row', gap: 20}} key={d.instructions}>
-              <Input defaultValue={d.instructions} label={`Direction ${_idx}`} name={`directions[${idx}].instructions`} ref={register()}/>
+              <Input defaultValue={d.instructions} label={`Direction ${_idx}`} name={`directions[${idx}].instructions`} ref={register({required: true})}/>
               <Input label={`Optional`} name={`directions[${idx}].optional`} type={"checkbox"} checked={d.optional} ref={register()} style={{margin: '0 auto'}}/>
                 <Button onClick={()=> removeDirection(idx)} text="remove">remove</Button>
             </div>
@@ -75,6 +79,20 @@ const EditRecipe: React.FC<Props> = ({ recipe }) => {
         {store.update_recipe.loading && <div style={{backgroundColor: 'thistle', padding: '5px'}}>Loading...</div>}
         {store.update_recipe.loaded && <div style={{backgroundColor: 'yellowgreen', padding: '5px'}}>Saved</div>}
         {store.update_recipe.error && <div style={{backgroundColor: 'tomato', padding: '5px'}}>An Error Occured</div>}
+        <div>
+        {errors && errors?.ingredients?.map(i=>{
+          if(i && i.name?.type === 'required'){
+            return <div className="warning">Ingredient Field can not be empty</div>
+          }
+        })}
+        </div>
+        <div>
+        {errors && errors?.directions?.map(i=>{
+          if(i && i.instructions?.type === 'required'){
+            return <div className="warning">Direction Field can not be empty</div>
+          }
+        })}
+        </div>
         <button>Submit?</button>
       </form>
     </div>
